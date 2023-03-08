5e2a995e33c5796dbe634e98533362a984ef7816
VAIBHAV SUTAR <VAIBHAV SUTAR@DESKTOP-EROM9LS>
2023-03-08 15:00:21 +0530

    2nd commit

---
+++ day1/src/day1/DataTypes.java
@@ -0,0 +1,20 @@
+package day1;
+
+public class DataTypes {
+
+public static void main(String[] args) {
+		int value1=9/2;  
+		float value2=101/101;   
+		double value3=10d/6d;
+		float value4= 5/2.5f;
+		
+		
+		System.out.println("value1=" + value1);
+		System.out.println("value2=" +value2);
+		System.out.println("value3=" +value3);
+		System.out.println("value4=" +value4);
+		
+
+	}
+
+}
---
+++ day1/src/day1/Identifier.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class Identifier {
+
+	public static void main(String[] args) {
+		
+		int fr =13;  // ! java keywords
+		int number_2=23; // no space between iden.
+		
+		int numbar3=10;  // not allows special charac.
+		
+		int $numbar=20;  // start with $ can..
+		
+		System.out.println("value of the numbar variable is : "+$numbar );
+		String studentsName="aniket";
+		System.out.println("studentsName");
+		
+		
+		
+
+	}
+
+}
